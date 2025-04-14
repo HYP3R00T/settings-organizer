fc6e9e2c900fc3837c70c46cadd5fdada0c4249c
hyperoot <rajesh@hyperoot.dev>
2025-04-14 02:17:51 +0000

    chore: re map import

--- src/utils/fileFinder.ts
+++ src/utils/fileFinder.ts
@@ -1,87 +1,87 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
-import { detectPlatform, getWindowsUsername } from "./system";
+import { detectPlatform, getWindowsUsername } from "@/utils/system";
 
 export async function getSettingsPath(): Promise<string | null> {
-  let defaultPath = getUserSettingsPath();
+    let defaultPath = getUserSettingsPath();
 
-  if (defaultPath && fs.existsSync(defaultPath)) {
-    return defaultPath;
-  } else {
-    return null;
-  }
+    if (defaultPath && fs.existsSync(defaultPath)) {
+        return defaultPath;
+    } else {
+        return null;
+    }
 }
 
 export async function getLocalSettingsPath(): Promise<string | null> {
-  const workspaceFolders = vscode.workspace.workspaceFolders;
-  if (!workspaceFolders) {
-    vscode.window.showErrorMessage("No workspace folder is open.");
-    return null;
-  }
-  const userSettingsPath = path.join(
-    workspaceFolders[0].uri.fsPath,
-    ".vscode",
-    "settings.json"
-  );
-  return userSettingsPath;
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+        vscode.window.showErrorMessage("No workspace folder is open.");
+        return null;
+    }
+    const userSettingsPath = path.join(
+        workspaceFolders[0].uri.fsPath,
+        ".vscode",
+        "settings.json"
+    );
+    return userSettingsPath;
 }
 
 export async function getManualPath(): Promise<string | null> {
-  let path: vscode.Uri[] | undefined = await vscode.window.showOpenDialog({
-    canSelectFiles: true,
-    canSelectMany: false,
-    canSelectFolders: false,
-  });
+    let path: vscode.Uri[] | undefined = await vscode.window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectMany: false,
+        canSelectFolders: false,
+    });
 
-  if (path) {
-    return path[0].path;
-  }
+    if (path) {
+        return path[0].path;
+    }
 
-  return null;
+    return null;
 }
 
 function isInsidersVersion() {
-  return vscode.version.includes("-insider");
+    return vscode.version.includes("-insider");
 }
 
 function getUserSettingsPath() {
-  const platform = detectPlatform();
-  let userSettingsPath = "";
-  if (platform === "win32") {
-    userSettingsPath = path.join(
-      process.env.APPDATA || "",
-      (isInsidersVersion() ? "Code - Insiders" : "Code"),
-      "User",
-      "settings.json"
-    );
-  } else if (platform === "darwin") {
-    userSettingsPath = path.join(
-      process.env.HOME || "",
-      "Library",
-      "Application Support",
-      (isInsidersVersion() ? "Code - Insiders" : "Code"),
-      "User",
-      "settings.json"
-    );
-  } else if (platform === "wsl2") {
-    const windowsUsername = getWindowsUsername();
-    if (windowsUsername) {
-      userSettingsPath = path.join(
-        "/mnt/c/Users",
-        windowsUsername,
-        (isInsidersVersion() ? "AppData/Roaming/Code - Insiders/User/settings.json" : "AppData/Roaming/Code/User/settings.json"),
-      );
+    const platform = detectPlatform();
+    let userSettingsPath = "";
+    if (platform === "win32") {
+        userSettingsPath = path.join(
+            process.env.APPDATA || "",
+            (isInsidersVersion() ? "Code - Insiders" : "Code"),
+            "User",
+            "settings.json"
+        );
+    } else if (platform === "darwin") {
+        userSettingsPath = path.join(
+            process.env.HOME || "",
+            "Library",
+            "Application Support",
+            (isInsidersVersion() ? "Code - Insiders" : "Code"),
+            "User",
+            "settings.json"
+        );
+    } else if (platform === "wsl2") {
+        const windowsUsername = getWindowsUsername();
+        if (windowsUsername) {
+            userSettingsPath = path.join(
+                "/mnt/c/Users",
+                windowsUsername,
+                (isInsidersVersion() ? "AppData/Roaming/Code - Insiders/User/settings.json" : "AppData/Roaming/Code/User/settings.json"),
+            );
+        }
+    } else if (platform === "linux") {
+        userSettingsPath = path.join(
+            process.env.HOME || "",
+            ".config",
+            (isInsidersVersion() ? "Code - Insiders" : "Code"),
+            "User",
+            "settings.json"
+        );
     }
-  } else if (platform === "linux") {
-    userSettingsPath = path.join(
-      process.env.HOME || "",
-      ".config",
-      (isInsidersVersion() ? "Code - Insiders" : "Code"),
-      "User",
-      "settings.json"
-    );
-  }
 
-  return userSettingsPath;
+    return userSettingsPath;
 }
